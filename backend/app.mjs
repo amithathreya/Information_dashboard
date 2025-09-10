a6d82f06d5d5f84468b2470b89928d13af1cf5b3
amithathreya <athreya.amith88@gmail.com>
2025-09-10 13:55:27 +0000

    change from next to react + vite

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,13 +1,20 @@
 import express from 'express'
+import path from 'path'
 
 
 const app = express()
 
+app.use(express.static(path.join(path.resolve(), 'public')))
+
+app.use(express.json())
 
 app.get('/',(req,res)=> {
     res.json({message:'hello world!'})
 })
-
-app.listen(3000 ,() => {
-    console.log('Server is running on http://localhost:3000')
+app.post('/data',(req,res) => {
+    const {name} = req.body
+    res.json({message:`Hello, ${name}!`})
+})
+app.listen(8080 ,() => {
+    console.log('Server is running on http://localhost:8080')
 })
