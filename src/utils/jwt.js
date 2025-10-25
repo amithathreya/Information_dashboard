eca576a5a5159b81e135642cc7d46224d421eaa9
amithathreya <athreya.amith88@gmail.com>
2025-10-25 17:42:51 +0530

    reduce jwt time

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,7 +2,7 @@
 import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
-const JWT_EXPIRES_IN = '2h';
+const JWT_EXPIRES_IN = '5m';
 
 export function signToken(payload) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
